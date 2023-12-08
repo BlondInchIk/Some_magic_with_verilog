ecfaed44080ffde84e233f5ebcf575b5d71c72a6
@ampavlenko <ampavlenko@miem.hse.ru>
2023-12-09 02:47:30 +0300

    1

--- DOP_TASK/testbench.v
+++ DOP_TASK/testbench.v
@@ -1,39 +1,83 @@
-module shift_reg_with_display_tb;
+// Testbench for shift_reg module
 
-reg CLOCK_50;
-reg [1:0] KEY;
-reg [0:0] SW;
-wire [9:0] LEDR;
-wire [7:0] HEX1;
-wire [7:0] HEX0;
+`timescale 1ns/1ns
 
-// подключение модуля shift_reg_with_display
-shift_reg_with_display dut (
-    .CLOCK_50(CLOCK_50),
-    .KEY(KEY),
-    .SW(SW),
-    .LEDR(LEDR),
-    .HEX1(HEX1),
-    .HEX0(HEX0)
-);
+module testbench;
+
+   localparam WIDTH=8;
+   
+   reg        clk;
+   reg        rst_n;
+   reg        shift_en;
+   reg        data_in;
+   wire [7:0] data_out;
+   wire       serial_out;
+      
+   
+   shift_reg #(.WIDTH(WIDTH)) shft
+   (// Outputs
+      .data_out   ( data_out   ),
+      .serial_out ( serial_out ),
+      // Inputs
+      .clk        ( clk        ),
+      .rst_n      ( rst_n      ),
+      .data_in    ( data_in    ),
+      .shift_en   ( shift_en   )
+   );
+
+   //Clock generation
+   initial begin
+      clk = 0;
+      forever #4 clk = ~clk;
+   end
+
+   // Create serial package
+   task push;
+      begin
+         $display("########## Shift enable active ##########");
+         shift_en  = 1'b1;
+         repeat(9)begin
+            @(negedge clk)
+              data_in = $urandom%2;
+         end
+         shift_en  = 1'b0;
+         $display("########## Shift enable disable ##########\n");
+      end
+   endtask
+
+   // Create reset signal
+   task async_rst;
+      begin
+         $display("########## RESET start! ##########");
+         #10 rst_n = 1'b0;
+         #10 rst_n = 1'b1;
+         $display("########## RESET finish!\n ##########");
+      end
+   endtask
+   
+   initial begin
+      $dumpvars();
+      $display("#################### Starting simulation ####################");
+      $monitor("@time=%0t\t data_out=0x%h\t",$time,data_out);
+      
+      shift_en  = 1'b0;
+      rst_n     = 1'b1;
+      data_in   = 1'b0;
+      
+      //Async reset
+      async_rst();
+      
+     //Push new data
+      repeat(10) begin
+         #50 push;
+      end      
+      #200 $finish;
+   end
 
 initial begin
-    clk = 0;
-    forever #5 clk = clk;
-end
-
-// генерация входных сигналов
-initial begin
-    KEY = 2'b00; // установка начальных значений ключей
-    SW = 1'b0; // установка начального значения переключателя
-    #10 KEY = 2'b01; // изменение значения ключей после некоторого времени
-    #100 $finish; // завершение симуляции после некоторого времени
-end
-
-  initial begin
     $dumpfile("dump.vcd");  // Запись VCD-файла для отладки
     $dumpvars(0, testbench);  // Запись переменных в VCD-файл
     $monitor($time, "out = %b", data_out);  // Вывод значения data_out во время симуляции
   end
-
-endmodule
+  
+endmodule // testbench

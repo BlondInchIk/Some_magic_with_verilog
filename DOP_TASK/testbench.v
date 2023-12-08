318c971b9133d68d312aa73326fbc9846e70ae35
@ampavlenko <ampavlenko@miem.hse.ru>
2023-12-09 02:14:15 +0300

    1

--- DOP_TASK/testbench.v
+++ DOP_TASK/testbench.v
@@ -1,27 +1,33 @@
-module testbench;
+module shift_reg_with_display_tb;
 
-reg clk;
-reg rst;
-wire [3:0] out;
+reg CLOCK_50;
+reg [1:0] KEY;
+reg [0:0] SW;
+wire [9:0] LEDR;
+wire [7:0] HEX1;
+wire [7:0] HEX0;
 
-shift_register dut(
-    .clk(clk),
-    .rst(rst),
-    .out(out)
+// подключение модуля shift_reg_with_display
+shift_reg_with_display dut (
+    .CLOCK_50(CLOCK_50),
+    .KEY(KEY),
+    .SW(SW),
+    .LEDR(LEDR),
+    .HEX1(HEX1),
+    .HEX0(HEX0)
 );
 
-initial begin
-    clk = 0;
-    forever #5 clk =  clk;
+// генерация тактового сигнала
+always begin
+    #5 CLOCK_50 = ~CLOCK_50;
 end
 
+// генерация входных сигналов
 initial begin
-    rst = 1;
-    #10 rst = 0;
-end
-
-initial begin
-    #20 finish;
+    KEY = 2'b00; // установка начальных значений ключей
+    SW = 1'b0; // установка начального значения переключателя
+    #10 KEY = 2'b01; // изменение значения ключей после некоторого времени
+    #100 $finish; // завершение симуляции после некоторого времени
 end
 
 endmodule

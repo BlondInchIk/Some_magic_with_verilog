1aedf8a82efea91f66f373b4501db8d55c1fba0f
@ampavlenko <ampavlenko@miem.hse.ru>
2023-12-08 04:01:05 +0300

    1

--- SISO/SISO.v
+++ SISO/SISO.v
@@ -1,16 +1,26 @@
-`timescale 1ns / 1ps
-module SISO (
-input wire clk,
-input wire b,
-input wire reset,
-output reg q
+module siso #(parameter DATA_WIDTH = 8) (
+    input wire clk,
+    input wire reset,
+    input wire enable,
+    input wire set_all_ones,
+    input wire data_in,
+    output reg data_out
 );
 
-always @(posedge clk) begin
-if (reset)
-q <= 1'b0;
-else
-q <= b;
+reg [DATA_WIDTH-1:0] shift_reg;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        shift_reg <= 0;
+    end else if (enable) begin
+        if (set_all_ones) begin
+            shift_reg <= {DATA_WIDTH{1'b1}};
+        end else begin
+            shift_reg <= {shift_reg[DATA_WIDTH-2:0], data_in};
+        end
+    end
 end
+
+assign data_out = shift_reg[DATA_WIDTH-1];
 
 endmodule
